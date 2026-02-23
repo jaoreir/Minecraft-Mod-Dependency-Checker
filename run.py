cca8580b91d94af13faef3da572866557ca74c74
Jaoreir <jaoreir@gmail.com>
2026-02-23 13:44:05 +0800

    refactor

--- run.py
+++ run.py
@@ -9,47 +9,52 @@ from rich import print as rprint
 from rich.tree import Tree
 
 
-def list_dependencies(mods_folder):
+def parse_toml(mods_info, file_name, jar_file, inner_file_name):
+    with jar_file.open(inner_file_name) as toml_file:
+        try:
+            content = toml_file.read().decode("utf-8")
+            data = tomllib.loads(content)
+            if "mods" in data and data["mods"]:
+                mod_id = data["mods"][0].get("modId", "unknown")
+                if mod_id != "unknown":
+                    dependencies = data.get("dependencies", {}).get(mod_id, [])
+                    mods_info[mod_id] = [
+                        dep["modId"]
+                        for dep in dependencies
+                        if dep["modId"] not in ("forge", "minecraft")
+                        # and dep.get("mandatory", False)
+                    ]
+        except tomllib.TOMLDecodeError as er:
+            print(er, file=sys.stderr)
+            print(f"Error parsing {file_name}", file=sys.stderr)
+            print(f"Will skip {file_name}!", file=sys.stderr)
+            return
+
+
+def parse_json(mods_info, file_name, jar_file, inner_file_name):
+    with jar_file.open(inner_file_name) as json_file:
+        data = json.load(json_file)
+        mod_id = data.get("id", "unknown")
+        if mod_id != "unknown":
+            dependencies = data.get("depends", {})
+            mods_info[mod_id] = [
+                dep for dep in dependencies.keys() if dep not in ("forge", "minecraft")
+            ]
+
+
+def parse_mods_info(mods_folder):
     mods_info = {}
 
     for file_name in os.listdir(mods_folder):
-        if file_name.endswith(".jar"):
-            jar_path = os.path.join(mods_folder, file_name)
-            with zipfile.ZipFile(jar_path, "r") as jar_file:
-                for name in jar_file.namelist():
-                    if name.endswith("mods.toml"):
-                        with jar_file.open(name) as toml_file:
-                            try:
-                                content = toml_file.read().decode("utf-8")
-                                data = tomllib.loads(content)
-                                if "mods" in data and data["mods"]:
-                                    mod_id = data["mods"][0].get("modId", "unknown")
-                                    if mod_id != "unknown":
-                                        dependencies = data.get("dependencies", {}).get(
-                                            mod_id, []
-                                        )
-                                        mods_info[mod_id] = [
-                                            dep["modId"]
-                                            for dep in dependencies
-                                            if dep["modId"]
-                                            not in ("forge", "minecraft")
-                                            # and dep.get("mandatory", False)
-                                        ]
-                            except tomllib.TOMLDecodeError as er:
-                                print(f"Error parsing {file_name}", file=sys.stderr)
-                                print(er, file=sys.stderr)
-                                continue
-                    elif name.endswith("fabric.mod.json"):
-                        with jar_file.open(name) as json_file:
-                            data = json.load(json_file)
-                            mod_id = data.get("id", "unknown")
-                            if mod_id != "unknown":
-                                dependencies = data.get("depends", {})
-                                mods_info[mod_id] = [
-                                    dep
-                                    for dep in dependencies.keys()
-                                    if dep not in ("forge", "minecraft")
-                                ]
+        if not file_name.endswith(".jar"):
+            continue
+        jar_path = os.path.join(mods_folder, file_name)
+        with zipfile.ZipFile(jar_path, "r") as jar_file:
+            for inner_file_name in jar_file.namelist():
+                if inner_file_name.endswith("mods.toml"):
+                    parse_toml(mods_info, file_name, jar_file, inner_file_name)
+                elif inner_file_name.endswith("fabric.mod.json"):
+                    parse_json(mods_info, file_name, jar_file, inner_file_name)
 
     return mods_info
 
@@ -123,7 +128,7 @@ def main():
         print("Path not provided!", file=sys.stderr)
         exit(1)
     mods_folder_path = sys.argv[1].strip()
-    mods_info = list_dependencies(mods_folder_path)
+    mods_info = parse_mods_info(mods_folder_path)
     # print_table(mods_info)
     print_tree(mods_info)
 
